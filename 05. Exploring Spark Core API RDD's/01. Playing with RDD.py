1456a548787eab7b68ebd3f756d36062f17c1cbb
naveenpn-trainer <naveenpn-trainer>
2024-06-20 05:15:47 +0000

    Added code for Exploring Spark Core

---
+++ 05. Exploring Spark Core API RDD's/01. Playing with RDD.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Operations
+# MAGIC
+# MAGIC There are two types of operations
+# MAGIC
+# MAGIC 1. Transformation
+# MAGIC 2. Actions
+
+# COMMAND ----------
+
+
